78329600f25e26e27d6a300f91a1fdd5aec514ce
baoliangwang <baoliangww@gmail.com>
2014-02-01 20:58:23 -0700

    camera lab

---
+++ gen/ualberta/cmput301/camerodemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ualberta.cmput301.camerodemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/ualberta/cmput301/camerademo/CameraDemoActivity.java
@@ -0,0 +1,55 @@
+package ualberta.cmput301.camerademo;
+
+import ualberta.cmput301.camerodemo.R;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class CameraDemoActivity extends Activity {
+
+	private TextView textView;
+	private ImageButton imageButton;
+	private Uri imageFileUri;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_camero_demo);
+		
+		// Retrieve handlers
+		textView = (TextView) findViewById(R.id.status);
+		imageButton = (ImageButton) findViewById(R.id.image);
+		
+		// Set up the listener
+		OnClickListener listener = new OnClickListener() {
+			public void onClick(View view) {
+				takeAPhoto(); // implement this method
+			}
+		};
+		// Register a callback to be invoked when this view is clicked
+		imageButton.setOnClickListener(listener);
+	}
+
+
+	// Implement takeAPhoto() method to allow you to take a photo when you click the ImageButton.
+	// Notice that startActivity() method will not return any result when the launched activity 
+	// finishes, while startActivityForResult() method will. To retrieve the returned result, you may 
+	// need implement onAcitityResult() method.
+	public void takeAPhoto() {
+		
+	}
+	
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.camero_demo, menu);
+		return true;
+	}
+
+}
